bb01b9bd08aa4d445db28f453c46f72d8554053a
paul-chrlt <paul.charlet@gmail.com>
2018-05-11 23:32:55 +0200

    export average tidy dataset

--- run_analysis.R
+++ run_analysis.R
@@ -68,3 +68,5 @@ datasetaverage <- dataset
 
 datasetaverage<-ddply(datasetaverage,c("subjectID","activity"),colwise(mean))
 names(datasetaverage)[3:length(datasetaverage[1,])] <- paste("mean of",names(datasetaverage)[3:length(datasetaverage[1,])])
+
+write.table(datasetaverage,file="datasetaverage.txt")
