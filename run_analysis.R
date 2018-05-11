e0d3709101654e5bb21af7cb3f98c6aef5726725
paul-chrlt <paul.charlet@gmail.com>
2018-05-11 17:06:44 +0200

    correcting import, merging, naming

--- run_analysis.R
+++ run_analysis.R
@@ -1,19 +1,56 @@
 ## variables
-trainingsetdirlocation <- "./UCI HAR Dataset/train/"
-testsetdirlocation <- "./UCI HAR Dataset/test/"
-testfileregexpattern <- "^[a-zA-Z]_test.txt"
-trainfileregexpattern <- "^[a-zA-Z]_train.txt"
+sourcedirlocation <- "./UCI HAR Dataset/"
+testfiles <- c(
+        "test/subject_test.txt",
+        "test/X_test.txt",
+        "test/y_test.txt"
+)
+
+trainfiles <- c(
+        "train/subject_train.txt",
+        "train/X_train.txt",
+        "train/y_train.txt"
+)
+
+## creating source files paths
+
+filepathactivitylabels <- paste0(sourcedirlocation,"activity_labels.txt")
+filepathfeatureslabels <- paste0(sourcedirlocation,"features.txt")
+
+filepathtest <- paste0(sourcedirlocation,testfiles)
+
+filepathtrain <- paste0(sourcedirlocation, trainfiles)
 
 ## getting the sets
-testsetfiles <- paste0(testsetdirlocation,list.files(path=testsetdirlocation,pattern=testfileregexpattern))
-trainingsetfiles <- paste0(trainingsetdirlocation,list.files(path=trainingsetdirlocation,pattern=trainfileregexpattern))
 
-testset <- lapply(testsetfiles,read.csv)
-trainset <- lapply(trainingsetfiles,read.csv)
+activitylabels <- read.table(filepathactivitylabels,sep=" ",header = FALSE,colClasses = c("integer","character"))
+features <- read.table(filepathfeatureslabels,sep=" ",header=FALSE,colClasses = c("integer","character"))
 
+subjecttest <- read.table(filepathtest[1])
+subjecttrain <- read.table(filepathtrain[1])
+xtest <- read.table(filepathtest[2])
+xtrain <- read.table(filepathtrain[2])
+ytest <- read.table(filepathtest[3])
+ytrain <- read.table(filepathtrain[3])
+
+testset <- cbind(subjecttest,xtest,ytest)
+trainset <- cbind(subjecttrain,xtrain,ytrain)
 
 ## merging the sets
 
+dataset <- rbind(testset,trainset)
+
+## adding the names
+
+names(dataset) <- c("subjectID",features[,2],"activity")
+
+## activity should be a factor
+
+for (x in activitylabels[,1]) {
+        dataset$activity <- gsub(activitylabels[x,1],activitylabels[x,2],dataset$activity)
+        
+}
+dataset$activity <- as.factor(dataset$activity)
 
 ## extraction of the mean and standard deviation for each measurement
 
